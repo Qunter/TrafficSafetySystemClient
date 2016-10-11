5e2ee1c01a6912d9e228149d9fee17f3509e3152
yufalu <429142406@qq.com>
2016-10-11 21:22:15 +0800

    update

--- app/src/main/java/com/jxut/trafficsafetysystem/UI/MainActivity.java
+++ app/src/main/java/com/jxut/trafficsafetysystem/UI/MainActivity.java
@@ -1,12 +1,9 @@
 package com.jxut.trafficsafetysystem.UI;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
